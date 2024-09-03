ff34b6bba0729286c4c827e1e5d125a7fc87324a
Artem <artemm3note@gmail.com>
2024-09-03 18:08:38 +0300

    Implemented simple configuration. Added an ability to configure app (a little).

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller'
 import { BlogModule } from './blog/blog.module'
 import { UserModule } from './user/user.module'
 import { LoggerMiddleware } from './utils/middleware/logger.middleware'
+import * as appConfig from './app.config.json'
 
 @Module({
     imports: [BlogModule, UserModule],
@@ -11,8 +12,10 @@ import { LoggerMiddleware } from './utils/middleware/logger.middleware'
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
-        consumer
-            .apply(LoggerMiddleware)
-            .forRoutes('*')
+        if (appConfig.logger) {
+            consumer
+                .apply(LoggerMiddleware)
+                .forRoutes('*')
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,44 @@ import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilter } from './utils/filters/http-exception.filter'
+import * as appConfig from './app.config.json'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    app.setGlobalPrefix('api')
-    app.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-            whitelist: true
-        })
-    )
-    app.useGlobalFilters(new HttpExceptionFilter())
-    // app.enableCors()
 
-    const config = new DocumentBuilder()
-        .setTitle('Blogs API')
-        .setDescription('Documentation for Blogs application')
-        .setVersion('1.0')
-        .addTag('Blogs')
-        .addTag('Users')
-        .build()
+    app.setGlobalPrefix(appConfig.globalPrefix ?? 'api')
 
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('api', app, document)
+    if (appConfig.validation) {
+        app.useGlobalPipes(
+            new ValidationPipe({
+                transform: true,
+                whitelist: true
+            })
+        )
+    }
 
-    await app.listen(3000)
+    if (appConfig.exceptionFilter) {
+        app.useGlobalFilters(new HttpExceptionFilter())
+    }
+
+    if (appConfig.cors) {
+        app.enableCors()
+    }
+
+    if (appConfig.swagger) {
+        const config = new DocumentBuilder()
+            .setTitle('Blogs API')
+            .setDescription('Documentation for Blogs application')
+            .setVersion('1.0')
+            .addTag('Blogs')
+            .addTag('Users')
+            .build()
+    
+        const document = SwaggerModule.createDocument(app, config)
+        SwaggerModule.setup('api', app, document)
+    }
+
+    await app.listen(appConfig.port ?? 3000)
     console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
